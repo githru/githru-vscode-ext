8cf352e4107dc81c64db2a81f6605aaab92b8494
snowy <chocoheim@gusty.local>
2022-08-07 18:42:21 +0900

    feat(vscode): launch webview for webviewApp

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import WebviewLoader from './webview-loader';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,10 +14,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('githru.command.open.webview', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode!');
+		new WebviewLoader(context.extensionUri, context.extensionPath, "View Contents Here");
+
+		vscode.window.showInformationMessage('Hello githru-vscode-ext :-)');
 	});
 
 	context.subscriptions.push(disposable);
