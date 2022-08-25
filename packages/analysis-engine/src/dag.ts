fb20beb6b687913a88e0f97777233554a7dd1500
조예진 <orobos654@gmail.com>
2022-08-26 02:11:27 +0900

    feat: implement dag

---
+++ packages/analysis-engine/src/dag.ts
@@ -0,0 +1,31 @@
+// TODO: git log parsing을 통해 생성되는 Commit 타입으로 대체
+interface Commit {
+  id: string;
+  parents: string[];
+  branches: string[];
+}
+
+export interface CommitDagNode {
+  id: string;
+  commit: Commit;
+}
+
+export type CommitDag = Map<string, CommitDagNode>;
+
+function generateDagNode(commit: Commit): CommitDagNode {
+  return {
+    id: commit.id,
+    parents: commit.parents,
+    commit,
+  } as CommitDagNode;
+}
+
+export function buildDag(commitList: Commit[]): CommitDag {
+  const dag: CommitDag = new Map<string, CommitDagNode>();
+
+  commitList
+    .map((commit) => generateDagNode(commit))
+    .forEach((node) => dag.set(node.id, node));
+
+  return dag;
+}
