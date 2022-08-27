a05fd866437a19fbdc7de10c97d3f46964a208e0
chaejung Kim <whenucan35@gmail.com>
2022-08-27 14:11:13 +0900

    chore: prettier lint apply

--- packages/analysis-engine/src/parseInterface.ts
+++ packages/analysis-engine/src/parseInterface.ts
@@ -1,31 +1,31 @@
 export interface CommitList {
-    // basic: 일반 commit
-    // merge: merge commit
-    type: string;
-    // basic : [현재 commit hash, parent commit hash]
-    // merge: [현재 commit hash, ,main stem에 위치한 commit hash, branched stem에 위치한 commit hash]
-    commitId: string;
-    parentIdList: string[];
-    author: string;
-    authorEmail: string;
-    authorDate: string;
-    committer: string;
-    committerEmail: string;
-    commitDate: string;
-    // view에 따라서 달라짐
-    message: string[];
-    // 파일 변경 사항
-    fileChanged: FileChanged;
-    branches: string[];
+  // basic: 일반 commit
+  // merge: merge commit
+  type: string;
+  // basic : [현재 commit hash, parent commit hash]
+  // merge: [현재 commit hash, ,main stem에 위치한 commit hash, branched stem에 위치한 commit hash]
+  commitId: string;
+  parentIdList: string[];
+  author: string;
+  authorEmail: string;
+  authorDate: string;
+  committer: string;
+  committerEmail: string;
+  commitDate: string;
+  // view에 따라서 달라짐
+  message: string[];
+  // 파일 변경 사항
+  fileChanged: FileChanged;
+  branches: string[];
 }
-  
+
 export interface FileChanged {
-    directory: Directory;
+  directory: Directory;
 }
 
 export interface Directory {
-    // 추가 라인 수
-    insertions: number;
-    // 삭제 라인 수
-    deletions: number;
+  // 추가 라인 수
+  insertions: number;
+  // 삭제 라인 수
+  deletions: number;
 }
