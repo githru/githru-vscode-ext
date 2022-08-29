604d776e880836219b7ebad8ca129652f5d1a69e
key <ansrlm@daum.net>
2022-08-29 21:06:48 +0900

    feat: types example

--- packages/view/src/components/Statistics/Statistics.tsx
+++ packages/view/src/components/Statistics/Statistics.tsx
@@ -1,5 +1,24 @@
+import type { DifferenceStatistic } from "@githru-vscode-ext/analysis-engine/src/types/CommitRaw";
+
+const mockDifferenceStatistic: DifferenceStatistic = {
+  totalInsertionCount: 300,
+  totalDeletionCount: 100,
+  fileDictionary: {
+    "./a": {
+      insertionCount: 140,
+      deletionCount: 30,
+    },
+    "./b": {
+      insertionCount: 160,
+      deletionCount: 70,
+    },
+  },
+};
+
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div>{mockDifferenceStatistic.fileDictionary["./a"].insertionCount}</div>
+  );
 };
 
 export default Statistics;
