115b81a6c241140d11d73c73f3cd3bb5b4419ecf
key <ansrlm@daum.net>
2022-08-29 21:23:22 +0900

    chore: typo

--- packages/vscode/src/webview-loader.ts
+++ packages/vscode/src/webview-loader.ts
@@ -7,7 +7,7 @@ export default class WebviewLoader {
     private fsPath: string;
     
     constructor(private readonly fileUri: vscode.Uri, private readonly extensionPath: string, content: string) {
-        console.log(content)
+        console.log(content);
         const viewColumn = vscode.ViewColumn.One;
 
         this.fsPath = fileUri.fsPath;
