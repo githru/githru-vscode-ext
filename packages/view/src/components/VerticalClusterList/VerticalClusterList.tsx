fc7d13da1ebae675b8de3f86305337be4ae9bd75
vgihan <jum654@naver.com>
2022-09-03 21:39:27 +0900

    feat(view): add Container Component in VerticalClusterList

--- packages/view/src/components/VerticalClusterList/VerticalClusterList.tsx
+++ packages/view/src/components/VerticalClusterList/VerticalClusterList.tsx
@@ -1,10 +1,11 @@
 import type { GlobalProps } from "types/global";
+import "./VerticalClusterList.scss";
 
 import { ClusterGraph } from "components/VerticalClusterList/ClusterGraph";
 
 const VerticalClusterList = ({ data }: GlobalProps) => {
   return (
-    <div>
+    <div className="vertical-cluster-list">
       <ClusterGraph data={data} />
     </div>
   );
