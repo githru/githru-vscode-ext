e7f5becff53e0782138492642b44afec74d53f17
jin-Pro <dnjun2@ajou.ac.kr>
2022-09-03 14:04:55 +0900

    enhance(view): add Commit Type Message Property closed [#70]
    enhance(view): CommitNode Type add taskId Property
    
    feat(view): Detail Component

---
+++ packages/view/src/components/Detail/Detail.util.ts
@@ -0,0 +1,34 @@
+import type { GlobalProps } from "types/global";
+
+import type { CommitNode } from "types/NodeTypes.temp";
+
+type GetTargetCommit = GlobalProps & { id: string };
+
+export const getTargetCommit = ({ data, id }: GetTargetCommit) => {
+  if (data?.length === 0) return undefined;
+  const flatCommitNode: CommitNode[] = data
+    .map((clusterNode) => clusterNode.commitNodeList)
+    .flat();
+  const [target] = flatCommitNode.filter(
+    (commitNode) => commitNode.commit.id === id
+  );
+
+  // parent Commit 추출하기
+  // const parentCommitId = target.commit.parentIds;
+  // console.log(parentCommitId);
+  // const parentCommits = parentCommitId.map((parentId) =>
+  //   flatCommitNode.filter(commitNode => commitNode.commit.id === parentId)
+  // );
+  // console.log(parentCommits);
+
+  // 동일한 Cluster Commit 추출하기
+  // const { taskId: ClusterId } = target;
+  // const clusterCommits = flatCommitNode.filter(
+  //   (commitNode) => commitNode.taskId === ClusterId
+  // );
+  // console.log(clusterCommits);
+  return target?.commit;
+};
+
+export const getTime = (date: Date) =>
+  String(date).split(".")[0].replace("T", " ");
