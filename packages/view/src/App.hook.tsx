1fe9f1d90434dec89be4d6fb34a9886c632c8ade
hslee08 <hslee08@cafe24corp.com>
2022-09-05 01:58:17 +0900

    chore(view): add return type of useGetTotalData function

--- packages/view/src/App.hook.tsx
+++ packages/view/src/App.hook.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 
-import type { ClusterNode } from "types";
+import type { ClusterNode, GlobalProps } from "types";
 
-export const useGetTotalData = () => {
+export const useGetTotalData = (): GlobalProps => {
   const [data, setData] = useState<ClusterNode[]>([]);
+
   useEffect(() => {
     fetch("/fake-assets/sampleClusterNodeList.json")
       .then((res) => res.json())
