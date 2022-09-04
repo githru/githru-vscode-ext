adcff5e3b28c8abec5e7b5dc67eb726ece817fee
key <ansrlm@daum.net>
2022-09-04 19:19:15 +0900

    fix: clear all test codes

--- packages/view/src/App.test.tsx
+++ packages/view/src/App.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
-import App from "./App";
+// import App from "./App";
 
 xtest("renders Welcome to githru as a text", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to githru./i);
-  expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  // const linkElement = screen.getByText(/Welcome to githru./i);
+  // expect(linkElement).toBeInTheDocument();
 });
