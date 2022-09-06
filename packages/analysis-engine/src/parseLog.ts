dfbb332c438db62c379d3512301621efa50e93c3
chaejung Kim <whenucan35@gmail.com>
2022-09-07 05:19:37 +0900

    refact(engine): PR comment 피드백 반영
    - slice를 startswith으로 대체했습니다.
    - if else 중첩을 줄이고 early return을 적용했습니다.
    - JSONArray 선언 코드 위치를 사용 직전으로 옮겼습니다.

--- packages/analysis-engine/src/parseLog.ts
+++ packages/analysis-engine/src/parseLog.ts
@@ -21,17 +21,13 @@ export default function parseToJSON(log: string) {
   const committers: GitUser[] = [];
   const commitDates: string[] = [];
   const messages: string[] = [];
-  // fileChanged의 경우 2개 이상이 될 수 있으므로 배열로 지정
   const differenceStatistics: DifferenceStatistic[] = [];
-
-  // 각 카테고리로 담은 다음 다시 JSON으로 변환하기 위함
-  const JSONArray: CommitRaw[] = [];
 
   // commit별 fileChanged를 분리시키기 위한 임시 index
   let commitIdx = -1;
   if (typeof splitByNewLine !== "undefined") {
     splitByNewLine.forEach((str, idx) => {
-      if (str.slice(0, 6) === "commit") {
+      if (str.startsWith("commit")) {
         commitIdx += 1;
         tags.push([]);
         branches.push([]);
@@ -44,48 +40,45 @@ export default function parseToJSON(log: string) {
         const commitInfos = splitedCommitLine[0]
           .replace("commit ", "")
           .split(" ")
-          .filter((commit) => commit !== "");
+          .filter((e) => e);
         ids.push(commitInfos[0]);
         commitInfos.splice(0, 1);
         parents.push(commitInfos);
         let branchAndTagsInfos = splitedCommitLine[1];
         if (typeof branchAndTagsInfos !== "undefined") {
-          if (branchAndTagsInfos.slice(0, 7) === "HEAD ->") {
+          if (branchAndTagsInfos.startsWith("HEAD ->")) {
             [, branchAndTagsInfos] = branchAndTagsInfos.split("HEAD ->");
           }
           branchAndTagsInfos.split(",").forEach((eachInfo) => {
-            if (eachInfo.trim().slice(0, 4) === "tag:") {
-              tags[commitIdx].push(eachInfo.replace("tag: ", "").trim());
-            } else {
-              branches[commitIdx].push(eachInfo.replace(")", "").trim());
-            }
+            if (eachInfo.trim().startsWith("tag:"))
+              return tags[commitIdx].push(eachInfo.replace("tag: ", "").trim());
+            return branches[commitIdx].push(eachInfo.replace(")", "").trim());
           });
         }
-      } else if (str.slice(0, 7) === "Author:") {
-        getNameAndEmail(authors, str);
-      } else if (str.slice(0, 10) === "AuthorDate") {
-        authorDates.push(str.split(": ")[1].trim());
-      } else if (str.slice(0, 7) === "Commit:") {
-        getNameAndEmail(committers, str);
-      } else if (str.slice(0, 10) === "CommitDate") {
+      }
+      if (str.startsWith("Author:")) return getNameAndEmail(authors, str);
+      if (str.startsWith("AuthorDate"))
+        return authorDates.push(str.split(": ")[1].trim());
+      if (str.startsWith("Commit:")) return getNameAndEmail(committers, str);
+      if (str.startsWith("CommitDate")) {
         let indexCheckFileChanged = idx + 2;
         let eachCommitMessage = "";
         while (splitByNewLine[indexCheckFileChanged] !== "") {
           if (eachCommitMessage !== "") {
             eachCommitMessage += "/n";
-            eachCommitMessage += splitByNewLine[indexCheckFileChanged].trim();
-          } else {
-            eachCommitMessage += splitByNewLine[indexCheckFileChanged].trim();
           }
+          eachCommitMessage += splitByNewLine[indexCheckFileChanged].trim();
           indexCheckFileChanged += 1;
         }
         commitDates.push(str.split(": ")[1].trim());
         messages.push(eachCommitMessage);
-      } else if (/^\d/.test(str) || /^-/.test(str)) {
+      }
+      if (/^\d/.test(str) || /^-/.test(str)) {
         const [addition, deletion, path] = str
           .split(" ")
           .filter((e) => e)[0]
           .split("\t");
+
         const numberedAddition = addition === "-" ? 0 : Number(addition);
         const numberedDeletion = deletion === "-" ? 0 : Number(deletion);
         differenceStatistics[commitIdx].totalInsertionCount += numberedAddition;
@@ -97,6 +90,9 @@ export default function parseToJSON(log: string) {
       }
     });
   }
+
+  // 각 카테고리로 담은 다음 다시 JSON으로 변환하기 위함
+  const JSONArray: CommitRaw[] = [];
 
   // 카테고리 별로 담은 것을 JSON화 시키기
   for (let i = 0; i < ids.length; i += 1) {
