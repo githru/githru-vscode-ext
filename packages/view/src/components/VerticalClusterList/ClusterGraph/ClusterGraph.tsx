ec9882cbc3b50a0cb8616e57b1d7031699893b15
vgihan <jum654@naver.com>
2022-09-07 16:25:17 +0900

    refactor(view): split draw func in ClusterGraph

--- packages/view/src/components/VerticalClusterList/ClusterGraph/ClusterGraph.tsx
+++ packages/view/src/components/VerticalClusterList/ClusterGraph/ClusterGraph.tsx
@@ -1,4 +1,6 @@
+import type { RefObject } from "react";
 import { useEffect, useRef } from "react";
+import type { Selection } from "d3";
 import { select } from "d3";
 
 import type { ClusterNode } from "types";
@@ -14,44 +16,72 @@ import {
   SVG_WIDTH,
 } from "./ClusterGraph.const";
 
+const drawClusterBox = (
+  container: Selection<SVGGElement, number, SVGSVGElement | null, unknown>
+) => {
+  container
+    .append("rect")
+    .attr("class", "cluster-box")
+    .attr("width", GRAPH_WIDTH)
+    .attr("height", COMMIT_HEIGHT)
+    .attr("x", SVG_MARGIN.left)
+    .attr("y", (_, i) => SVG_MARGIN.bottom + i * (NODE_GAP + COMMIT_HEIGHT));
+};
+
+const drawDegreeBox = (
+  container: Selection<SVGGElement, number, SVGSVGElement | null, unknown>,
+  maxOfClusterSize: number
+) => {
+  container
+    .append("rect")
+    .attr("class", "degree-box")
+    .attr("width", (d) => GRAPH_WIDTH * (d / maxOfClusterSize))
+    .attr("height", COMMIT_HEIGHT)
+    .attr(
+      "x",
+      (d) => (GRAPH_WIDTH * (1 - d / maxOfClusterSize)) / 2 + SVG_MARGIN.left
+    )
+    .attr("y", (_, i) => SVG_MARGIN.bottom + i * (NODE_GAP + COMMIT_HEIGHT));
+};
+
+const drawClusterGraph = (
+  svgRef: RefObject<SVGSVGElement>,
+  data: ClusterNode[]
+) => {
+  const clusterSizes = getClusterSizes(data);
+  const maxOfClusterSize = Math.max(...clusterSizes);
+
+  const group = select(svgRef.current)
+    .selectAll("g")
+    .data(clusterSizes)
+    .enter()
+    .append("g")
+    .attr("class", "cluster-container");
+
+  drawClusterBox(group);
+  drawDegreeBox(group, maxOfClusterSize);
+};
+
+const destroyClusterGraph = (target: RefObject<SVGElement>) => {
+  select(target.current).selectAll("svg").remove();
+};
+
 type ClusterGraphProps = {
   data: ClusterNode[];
 };
 
 const ClusterGraph = ({ data }: ClusterGraphProps) => {
-  const svgRef = useRef(null);
-
+  const svgRef = useRef<SVGSVGElement>(null);
   const clusterSizes = getClusterSizes(data);
   const graphHeight = getGraphHeight(clusterSizes);
-  const maxOfClusterSize = Math.max(...clusterSizes);
 
   useEffect(() => {
-    const group = select(svgRef.current)
-      .selectAll("g")
-      .data(clusterSizes)
-      .enter()
-      .append("g")
-      .attr("class", "cluster-container");
+    drawClusterGraph(svgRef, data);
 
-    group
-      .append("rect")
-      .attr("class", "cluster-box")
-      .attr("width", GRAPH_WIDTH)
-      .attr("height", COMMIT_HEIGHT)
-      .attr("x", SVG_MARGIN.left)
-      .attr("y", (_, i) => SVG_MARGIN.bottom + i * (NODE_GAP + COMMIT_HEIGHT));
-
-    group
-      .append("rect")
-      .attr("class", "degree-box")
-      .attr("width", (d) => GRAPH_WIDTH * (d / maxOfClusterSize))
-      .attr("height", COMMIT_HEIGHT)
-      .attr(
-        "x",
-        (d) => (GRAPH_WIDTH * (1 - d / maxOfClusterSize)) / 2 + SVG_MARGIN.left
-      )
-      .attr("y", (_, i) => SVG_MARGIN.bottom + i * (NODE_GAP + COMMIT_HEIGHT));
-  }, [clusterSizes, maxOfClusterSize]);
+    return () => {
+      destroyClusterGraph(svgRef);
+    };
+  }, [data]);
 
   return <svg ref={svgRef} width={SVG_WIDTH} height={graphHeight} />;
 };
