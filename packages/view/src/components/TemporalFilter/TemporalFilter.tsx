318cf3c06feaf5244ed3fea1c7d21465e8aef907
jin-Pro <dnjun2@ajou.ac.kr>
2022-09-08 02:11:47 +0900

    enhance(view): add FilteredData & SelectedData

--- packages/view/src/components/TemporalFilter/TemporalFilter.tsx
+++ packages/view/src/components/TemporalFilter/TemporalFilter.tsx
@@ -1,12 +1,19 @@
+import React from "react";
+
+import type { ClusterNode } from "types";
 import type { GlobalProps } from "types/global";
 
 import { ClocLineChart } from "./ClocLineChart";
 import { CommitLineChart } from "./CommitLineChart";
 import { sortBasedOnCommitNode } from "./TemporalFilter.util";
 
-const TemporalFilter = ({ data }: GlobalProps) => {
-  const sortedData = sortBasedOnCommitNode(data);
+type Props = GlobalProps & {
+  setFilteredData: React.Dispatch<React.SetStateAction<ClusterNode[]>>;
+};
 
+const TemporalFilter = ({ data, setFilteredData }: Props) => {
+  console.log(setFilteredData);
+  const sortedData = sortBasedOnCommitNode(data);
   return (
     <section>
       <ClocLineChart data={sortedData} />
