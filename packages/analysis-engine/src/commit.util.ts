e2153053a6cc4ae749b28d5586868334c2fa1829
조예진 <orobos654@gmail.com>
2022-09-08 19:50:25 +0900

    feat(engine): Implement STEM (#91)
    * feat: implement Stem
    
    * feat: change implicit branch stem naming
    
    * test: add test for HEAD
    
    * chore: set off the eslint no-continue rule
    
    * refactor: 리뷰 반영
    
    * feat: queue 생성 시, constructor에 정렬 비교 함수 파라미터 추가
    
    * chore: edit test name

---
+++ packages/analysis-engine/src/commit.util.ts
@@ -0,0 +1,18 @@
+import { CommitNode } from "./types/CommitNode";
+import { CommitRaw } from "./types/CommitRaw";
+
+type CommitDict = Map<string, CommitNode>;
+
+export function generateCommitNodeDict(commits: CommitRaw[]): CommitDict {
+  return new Map(
+    commits.map((commit) => [commit.id, { commit } as CommitNode])
+  );
+}
+
+export function getLeafNodes(commitDict: CommitDict): CommitNode[] {
+  const leafNodes: CommitNode[] = [];
+  commitDict.forEach(
+    (node) => node.commit.branches.length && leafNodes.push(node)
+  );
+  return leafNodes;
+}
