3dc39bc4093d68541aa5ef227a9b2cc38eeea6cf
jeonghye-choi <ppllhm.0@gmail.com>
2022-09-09 22:32:44 +0900

    feat: add Filter with date
    #32

--- packages/view/src/components/TemporalFilter/Filter/Filter.tsx
+++ packages/view/src/components/TemporalFilter/Filter/Filter.tsx
@@ -1,12 +1,52 @@
-const Filter = () => {
+import { useState } from "react";
+import * as React from "react";
+
+import "./Filter.scss";
+
+type Props = {
+  setFromDate: React.Dispatch<React.SetStateAction<string>>;
+  setToDate: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Filter = (props: Props) => {
+  const { setFromDate, setToDate } = props;
+  const [fromDateFilter, setFromDateFilter] = useState<string>("");
+  const [toDateFilter, setToDateFilter] = useState<string>("");
+
+  const fromDateChangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setFromDateFilter(e.target.value);
+    setFromDate(fromDateFilter);
+  };
+  const toDateChangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setToDateFilter(e.target.value);
+    setToDate(toDateFilter);
+  };
+
   return (
-    <form>
-      <input type="date" />
-      <input type="date" />
-      <input type="text" />
-      <input type="text" />
-      <button type="submit">Filtering</button>
-    </form>
+    <section className="filter">
+      <form>
+        <div>
+          <span>From:</span>
+          <input
+            className="date-from"
+            type="date"
+            value={fromDateFilter}
+            onChange={fromDateChangeHandler}
+          />
+          <span>To:</span>
+          <input
+            className="date-to"
+            type="date"
+            value={toDateFilter}
+            onChange={toDateChangeHandler}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
