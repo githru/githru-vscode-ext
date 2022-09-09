b3ec422d0714e2f70067005b766712dd8bd6f965
jeonghye-choi <ppllhm.0@gmail.com>
2022-09-09 22:33:24 +0900

    feat: add `filterDataByDate` util function

--- packages/view/src/components/TemporalFilter/TemporalFilter.util.ts
+++ packages/view/src/components/TemporalFilter/TemporalFilter.util.ts
@@ -4,6 +4,9 @@ import type { ClusterNode } from "types/NodeTypes.temp";
 
 import type { CommitNode } from "./TemporalFilter.type";
 
+/**
+ * Note: Line Chart를 위한 시간순 CommitNode 정렬
+ */
 export function sortBasedOnCommitNode(data: ClusterNode[]): CommitNode[] {
   const sortedData: CommitNode[] = [];
   data.forEach((cluster) => {
@@ -12,12 +15,38 @@ export function sortBasedOnCommitNode(data: ClusterNode[]): CommitNode[] {
 
   return Array.from(
     sortedData.sort((a, b) => {
-      return a.commit.commitDate > b.commit.commitDate ? 1 : -1;
+      return new Date(a.commit.commitDate) > new Date(b.commit.commitDate)
+        ? 1
+        : -1;
     })
   );
 }
 
-// TODO: 음수값 반영
+type FilterDataByDateProps = {
+  data: ClusterNode[];
+  fromDate: string;
+  toDate: string;
+};
+/**
+ * Note: 날짜 범위에 따라 필터링
+ */
+export function filterDataByDate(props: FilterDataByDateProps): ClusterNode[] {
+  const { data, fromDate, toDate } = props;
+
+  const filteredData = data.filter((clusterNode) => {
+    return clusterNode.commitNodeList.filter((commitNode) => {
+      if (
+        new Date(commitNode.commit.commitDate) >= new Date(fromDate) &&
+        new Date(commitNode.commit.commitDate) <= new Date(toDate)
+      ) {
+        return true;
+      }
+      return false;
+    });
+  });
+  return filteredData;
+}
+
 export const getCloc = (d: CommitNode) =>
   d.commit.diffStatistics.insertions + d.commit.diffStatistics.deletions;
 
