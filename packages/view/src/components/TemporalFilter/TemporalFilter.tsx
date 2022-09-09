120c0b229e2e9e1ef17c2d3b56c2cd09752da719
jeonghye-choi <ppllhm.0@gmail.com>
2022-09-09 22:35:39 +0900

    feat: add Filter, ThemeSelector to TemporalFilter
    #32

--- packages/view/src/components/TemporalFilter/TemporalFilter.tsx
+++ packages/view/src/components/TemporalFilter/TemporalFilter.tsx
@@ -1,24 +1,46 @@
-import React from "react";
+import * as React from "react";
+import { useState, useEffect } from "react";
 
 import type { ClusterNode } from "types";
 import type { GlobalProps } from "types/global";
 
 import { ClocLineChart } from "./ClocLineChart";
 import { CommitLineChart } from "./CommitLineChart";
-import { sortBasedOnCommitNode } from "./TemporalFilter.util";
+import { Filter } from "./Filter";
+import { filterDataByDate, sortBasedOnCommitNode } from "./TemporalFilter.util";
+import { ThemeSelector } from "./ThemeSelector";
+
+import "./TemporalFilter.scss";
 
 type Props = GlobalProps & {
   setFilteredData: React.Dispatch<React.SetStateAction<ClusterNode[]>>;
 };
 
 const TemporalFilter = ({ data, setFilteredData }: Props) => {
-  console.log(setFilteredData);
   const sortedData = sortBasedOnCommitNode(data);
+  const [fromDate, setFromDate] = useState<string>("");
+  const [toDate, setToDate] = useState<string>("");
+
+  useEffect(() => {
+    if (fromDate === "" || toDate === "") {
+      setFilteredData(data);
+    } else {
+      const filteredData = filterDataByDate({ data, fromDate, toDate });
+      setFilteredData(filteredData);
+    }
+  }, [fromDate, toDate]);
+
   return (
-    <section>
-      <ClocLineChart data={sortedData} />
-      <CommitLineChart data={sortedData} />
-    </section>
+    <article className="temporal-filter">
+      <div className="data-control-container">
+        <Filter setFromDate={setFromDate} setToDate={setToDate} />
+        <ThemeSelector />
+      </div>
+      <div className="line-chart">
+        <ClocLineChart data={sortedData} />
+        <CommitLineChart data={sortedData} />
+      </div>
+    </article>
   );
 };
 
