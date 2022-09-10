94b572d5d1e89ad4c23b54dfa6e3b8de7ade2c72
key <ansrlm@daum.net>
2022-09-10 19:50:35 +0900

    fix: migrate into react-app-env

--- packages/view/src/index.tsx
+++ packages/view/src/index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import type { ClusterNode } from "types";
-
 import App from "./App";
 import "styles/app.scss";
-
-declare global {
-  interface Window {
-    acquireVsCodeApi(): unknown;
-    githruData: ClusterNode[];
-    isProduction: boolean;
-  }
-}
 
 const container = document.getElementById("root") as HTMLElement;
 
--- packages/view/src/react-app-env.d.ts
+++ packages/view/src/react-app-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="react-scripts" />
 
 interface Window {
+  acquireVsCodeApi: () => unknown;
   githruData: unknown;
+  isProduction: boolean;
 }
