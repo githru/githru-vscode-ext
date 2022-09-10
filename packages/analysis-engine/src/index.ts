64b1098bcaf0e681e2e0edecd853484b44735073
key <ansrlm@daum.net>
2022-09-10 16:53:35 +0900

    feat: treat real function from parser

--- packages/analysis-engine/src/index.ts
+++ packages/analysis-engine/src/index.ts
@@ -1,18 +1,15 @@
+import { getCommitRaws, getGitLog } from "./parser";
 import { buildCommitDict } from "./commit.util";
 import { buildStemDict } from "./stem";
-import { CommitRaw } from "./types";
+// TODO: import { buildCSMDict } from "./csm"
 
 type AnalysisEngineOption = {
   isDebugMode: boolean;
 };
 
 export const analyzeGit = async (option?: AnalysisEngineOption) => {
-  // const gitLog = await getGitLog();
-  // const commitRaws = await parseToJSON(gitLog);
-
-  // const gitLog = await getGitLog();
-  // const commitRaws = await getCommitRaws(gitLog);
-  const commitRaws: CommitRaw[] = [];
+  const gitLog = await getGitLog();
+  const commitRaws = await getCommitRaws(gitLog);
   const commitDict = buildCommitDict(commitRaws);
   const stemDict = buildStemDict(commitDict);
   // const csmDict = buildCSM(commitDict, stemDict);
