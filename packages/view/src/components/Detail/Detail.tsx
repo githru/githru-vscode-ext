d251e555cebf291e93e1d47836f51b63ff1febe8
jin-Pro <dnjun2@ajou.ac.kr>
2022-09-11 13:36:15 +0900

    style(view): add Detail css

--- packages/view/src/components/Detail/Detail.tsx
+++ packages/view/src/components/Detail/Detail.tsx
@@ -2,6 +2,8 @@ import type { SelectedDataProps } from "types";
 
 import { getCommitListDetail } from "./Detail.util";
 
+import "./Detail.scss";
+
 const Detail = ({ selectedData }: { selectedData: SelectedDataProps }) => {
   if (!selectedData) return null;
   const commitNodeListInCluster = selectedData.commitNodeList;
@@ -10,14 +12,24 @@ const Detail = ({ selectedData }: { selectedData: SelectedDataProps }) => {
 
   return (
     <>
-      {commitNodeListInCluster.map((commitNode) => (
-        <div key={commitNode.commit.id}>{commitNode.commit.message}</div>
-      ))}
-      <div>
-        Excluding merges, {authorLength} authors have pushed {commitLength}
-        commits to main. On main, {fileLength} files have changed and there have
-        been
-        {insertions} additions and {deletions} deletions.
+      <ul className="detail-commit_item_container">
+        {commitNodeListInCluster.map((commitNode) => (
+          <li key={commitNode.commit.id}>{commitNode.commit.message}</li>
+        ))}
+      </ul>
+      <div className="detail-summary_container">
+        Excluding merges,
+        <span className="detail-summary_impact"> {authorLength} authors</span>
+        have pushed
+        <span className="detail-summary_impact"> {commitLength} commits</span>
+        to main. On main,
+        <span className="detail-summary_impact"> {fileLength} files </span>
+        have changed and there have been
+        <span className="detail-summary_insersions"> {insertions} </span>
+        <span className="detail-summary_impact">additions</span>
+        and
+        <span className="detail-summary_deletions"> {deletions} </span>
+        <span className="detail-summary_impact">deletions.</span>
       </div>
     </>
   );
