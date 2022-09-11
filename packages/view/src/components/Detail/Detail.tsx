26c2c31c796f8ab63b8f1222acd7a4e237514e22
jin-Pro <dnjun2@ajou.ac.kr>
2022-09-11 17:37:08 +0900

    fix(view): fix Detail Component space && naming

--- packages/view/src/components/Detail/Detail.tsx
+++ packages/view/src/components/Detail/Detail.tsx
@@ -19,14 +19,14 @@ const Detail = ({ selectedData }: { selectedData: SelectedDataProps }) => {
       </ul>
       <div className="detail-summary_container">
         Excluding merges,
-        <span className="detail-summary_impact"> {authorLength} authors</span>
+        <span className="detail-summary_impact"> {authorLength} authors </span>
         have pushed
-        <span className="detail-summary_impact"> {commitLength} commits</span>
+        <span className="detail-summary_impact"> {commitLength} commits </span>
         to main. On main,
         <span className="detail-summary_impact"> {fileLength} files </span>
         have changed and there have been
-        <span className="detail-summary_insersions"> {insertions} </span>
-        <span className="detail-summary_impact">additions</span>
+        <span className="detail-summary_insertions"> {insertions} </span>
+        <span className="detail-summary_impact">additions </span>
         and
         <span className="detail-summary_deletions"> {deletions} </span>
         <span className="detail-summary_impact">deletions.</span>
