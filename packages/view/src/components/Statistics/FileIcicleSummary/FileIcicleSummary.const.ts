577dbc9fe02c93bca0466c5544b516d26b43bad0
taejs <taeshindev@gmail.com>
2022-09-13 03:57:27 +0900

    style(view): change color code of FileIcicleSummary
    Following main color
    https://github.com/githru/githru-vscode-ext/issues/84

--- packages/view/src/components/Statistics/FileIcicleSummary/FileIcicleSummary.const.ts
+++ packages/view/src/components/Statistics/FileIcicleSummary/FileIcicleSummary.const.ts
@@ -5,6 +5,6 @@ export const MAX_DEPTH = 4;
 export const SINGLE_RECT_WIDTH = WIDTH / MAX_DEPTH;
 export const LABEL_VISIBLE_HEIGHT = 16;
 export const COLOR_CODE = {
-  dir: "#ffcc80",
-  file: "#ffe082",
+  dir: "#0077aa",
+  file: "#4DA0C4",
 };
