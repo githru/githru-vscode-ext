b02d89ddb94bab0f4c0bf2b7ff7d1cdd28542f20
제제jeje <gkswlgp456@gmail.com>
2022-10-02 01:42:13 +0900

    feat(view): show background color if no image

--- packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
+++ packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
@@ -1,19 +1,26 @@
+import React from "react";
+
 import type { AuthorProps } from "../Summary.type";
 import { getColorValue } from "../Summary.util";
 
 const Author = ({ name }: AuthorProps) => {
   const colorValue = getColorValue(name);
 
+  const url = `https://github.com/${name}.png`;
+
   return (
-    <span
-      className={["name"].join(" ")}
-      data-tooltip-text={name}
-      style={{
-        background: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), center / cover no-repeat url("https://github.com/${name}.png"),
-        ${colorValue}`,
-      }}
-    >
-      {`${name.slice(0, 1)}`}
+    <span className="name" data-tooltip-text={name}>
+      <img
+        className={["name"].join(" ")}
+        data-text={name.slice(0, 1)}
+        src={url}
+        alt=""
+        onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+          e.currentTarget.style.filter = "none";
+          e.currentTarget.style.overflow = "hidden";
+          e.currentTarget.style.background = colorValue;
+        }}
+      />
     </span>
   );
 };
