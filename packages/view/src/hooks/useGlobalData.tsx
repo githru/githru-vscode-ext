e88772626a4ba0d5674d46a2a5637d1a72a8043d
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-02 05:23:51 +0900

    feat: add useGlobalData Hook by tae.js pull#106

---
+++ packages/view/src/hooks/useGlobalData.tsx
@@ -0,0 +1,49 @@
+import type { Dispatch, ReactNode } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+import type { ClusterNode } from "../types";
+import { useGetTotalData } from "../App.hook";
+
+type GlobalDataState = Partial<{
+  data: ClusterNode[];
+  filteredData: ClusterNode[];
+  setFilteredData: Dispatch<ClusterNode[]>;
+  selectedData: ClusterNode[];
+  setSelectedData: Dispatch<ClusterNode[]>;
+}>;
+
+export const GlobalDataContext = createContext<GlobalDataState>(
+  {} as GlobalDataState
+);
+
+export const GlobalDataProvider = ({ children }: { children: ReactNode }) => {
+  const { data } = useGetTotalData();
+  const [filteredData, setFilteredData] = useState<ClusterNode[]>(data);
+  const [selectedData, setSelectedData] = useState<ClusterNode[]>([]);
+
+  useEffect(() => {
+    setSelectedData([]);
+  }, [filteredData]);
+
+  const value = useMemo(
+    () => ({
+      data,
+      filteredData,
+      setFilteredData,
+      selectedData,
+      setSelectedData,
+    }),
+    [data, filteredData, selectedData]
+  );
+
+  return (
+    <GlobalDataContext.Provider value={value}>
+      {children}
+    </GlobalDataContext.Provider>
+  );
+};
+
+export const useGlobalData = () => {
+  const globalData = useContext<GlobalDataState>(GlobalDataContext);
+  return globalData;
+};
