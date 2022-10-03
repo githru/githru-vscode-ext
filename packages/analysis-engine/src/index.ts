a685d39388e7410ed04b80bfb45a226b9f9a3fab
anpaul0615 <anpaul0615@gmail.com>
2022-10-04 02:25:32 +0900

    feat(engine): add pr based csm

--- packages/analysis-engine/src/index.ts
+++ packages/analysis-engine/src/index.ts
@@ -51,7 +51,12 @@ export class AnalysisEngine {
     if (this.isDebugMode) console.log("pullRequests: ", pullRequests);
     const stemDict = buildStemDict(commitDict, this.baseBranchName);
     if (this.isDebugMode) console.log("stemDict: ", stemDict);
-    const csmDict = buildCSMDict(commitDict, stemDict, this.baseBranchName);
+    const csmDict = buildCSMDict(
+      commitDict,
+      stemDict,
+      this.baseBranchName,
+      pullRequests
+    );
     if (this.isDebugMode) console.log("csmDict: ", csmDict);
 
     return csmDict;
