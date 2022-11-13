ad0436110ede015726f88292ac0de7b0c235f4bd
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 00:36:48 +0900

    fix(view): Author Image src state used useRef insteadOf useState && image size optimization

--- packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
+++ packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useRef } from "react";
 import md5 from "md5";
 
 import type { AuthorProps } from "../Summary.type";
 import { GITHUB_URL, GRAVATA_URL } from "../Summary.const";
 
 const Author = ({ name }: AuthorProps) => {
-  const src = `${GITHUB_URL}/${name}.png`;
+  const src = `${GITHUB_URL}/${name}.png?size=30`;
   const fallback = `${GRAVATA_URL}/${md5(name)}?d=identicon&f=y`;
+  const imgRef = useRef<HTMLImageElement>(null);
 
-  const [imgSrc, setImgSrc] = useState<string>(src);
-  const onError = () => setImgSrc(fallback);
+  const onError = () => {
+    if (imgRef.current) imgRef.current.src = fallback;
+  };
 
   return (
     <div className="name" data-tooltip-text={name}>
-      <img src={imgSrc} onError={onError} alt="" />
+      <img ref={imgRef} src={src} onError={onError} alt="profileImage" />
     </div>
   );
 };
