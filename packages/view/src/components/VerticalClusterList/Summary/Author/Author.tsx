2d4c7a1bf06bcd0b378fb5f445ed1103f22bb8f2
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-17 00:43:56 +0900

    enhance: Author Component Props Src

--- packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
+++ packages/view/src/components/VerticalClusterList/Summary/Author/Author.tsx
@@ -1,21 +1,9 @@
-import { useRef } from "react";
-import md5 from "md5";
-
 import type { AuthorProps } from "../Summary.type";
-import { GITHUB_URL, GRAVATA_URL } from "../Summary.const";
 
-const Author = ({ name }: AuthorProps) => {
-  const src = `${GITHUB_URL}/${name}.png?size=30`;
-  const fallback = `${GRAVATA_URL}/${md5(name)}?d=identicon&f=y`;
-  const imgRef = useRef<HTMLImageElement>(null);
-
-  const onError = () => {
-    if (imgRef.current) imgRef.current.src = fallback;
-  };
-
+const Author = ({ name, src }: AuthorProps) => {
   return (
     <div className="name" data-tooltip-text={name}>
-      <img ref={imgRef} src={src} onError={onError} alt="" />
+      <img src={src} alt="" />
     </div>
   );
 };
