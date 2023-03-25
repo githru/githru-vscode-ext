5a4e529c09e07c4bed39ef94bcf350f7c6f7b5a9
Seyoung Park <53702978+seyoung8239@users.noreply.github.com>
2023-03-25 19:42:18 +0900

    feat(vscode): add refresh handler (#292)
    * feat(vscode): add onRefresh logic
    
    * fix(vscode): revise refetch git log logic

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -23,18 +23,30 @@ export function activate(context: vscode.ExtensionContext) {
         const configuration = vscode.workspace.getConfiguration();
         const githubToken: string | undefined = configuration.get("githru.github.token");
         console.log("GitHubToken: ", githubToken);
-        const gitLog = await getGitLog(gitPath, currentWorkspacePath);
-        const gitConfig = await getGitConfig(gitPath, currentWorkspacePath, "origin");
-        const { owner, repo } = getRepo(gitConfig);
-        const branchNames = await getBranchNames(gitPath, currentWorkspacePath);
-        const baseBranchName = getBaseBranchName(branchNames);
 
-        const engine = new AnalysisEngine({ isDebugMode: true, gitLog, owner, repo, auth: githubToken, baseBranchName });
-        const csmDict = await engine.analyzeGit();
-        const clusterNodes = mapClusterNodesFrom(csmDict);
-        const data = JSON.stringify(clusterNodes);
+        const fetchClusterNodes = async () => {
+            const gitLog = await getGitLog(gitPath, currentWorkspacePath);
+            const gitConfig = await getGitConfig(gitPath, currentWorkspacePath, "origin");
+            const { owner, repo } = getRepo(gitConfig);
+            const branchNames = await getBranchNames(gitPath, currentWorkspacePath);
+            const baseBranchName = getBaseBranchName(branchNames);
+            const engine = new AnalysisEngine({
+                isDebugMode: true,
+                gitLog,
+                owner,
+                repo,
+                auth: githubToken,
+                baseBranchName,
+            });
+            const csmDict = await engine.analyzeGit();
+            const clusterNodes = mapClusterNodesFrom(csmDict);
+            const data = JSON.stringify(clusterNodes);
+            return data;
+        };
+        const initialData = await fetchClusterNodes();
+        const webLoader = new WebviewLoader(extensionUri, extensionPath, initialData, fetchClusterNodes);
 
-        subscriptions.push(new WebviewLoader(extensionUri, extensionPath, data));
+        subscriptions.push(webLoader);
 
         vscode.window.showInformationMessage("Hello Githru");
     });
