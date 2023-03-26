be16deb3e089d97cc26c731a94a67d452668e40a
이채준 - CJ Lee <chaejunlee@knu.ac.kr>
2023-03-26 09:39:53 +0900

    fix(view): better responsiveness with flexbox (#300)
    * fix(view): make statistics disappear under 850px viewport #288
    
    * fix(view): remove hard coded width value #288
    
    * fix(view): make commit-message ellipsis reponsive #288

--- packages/view/src/components/VerticalClusterList/Summary/Content/Content.tsx
+++ packages/view/src/components/VerticalClusterList/Summary/Content/Content.tsx
@@ -9,10 +9,12 @@ const Content = ({ content, clusterId, selectedClusterId }: ContentProps) => {
   return (
     <>
       <div className="cluster-summary__contents">
-        <span className="commit-message">{content.message}</span>
-        <span className="more-commit-count">
-          {content.count > 0 && `+ ${content.count} more`}
-        </span>
+        <div className="commit-message__wrapper">
+          <div className="commit-message">{content.message}</div>
+        </div>
+        {content.count > 0 && (
+          <span className="more-commit-count">+ {content.count} more</span>
+        )}
       </div>
       <div className="collapsible-icon">
         {selectedClusterId.includes(clusterId) ? (
