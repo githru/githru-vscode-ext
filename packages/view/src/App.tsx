09cf5ddaa53120063ccea9b270928e72e73d24f5
yewon0804 <ehdnjs0804@gmail.com>
2023-05-14 03:04:41 +0900

    feat(view): 웹뷰에서 데이터 가져오는 동안 로딩 UI 표시 (#332)

--- packages/view/src/App.tsx
+++ packages/view/src/App.tsx
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { container } from "tsyringe";
 import { useRef } from "react";
+import type { CSSProperties } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 
 import {
   BranchSelector,
@@ -15,18 +17,33 @@ import { useGlobalData } from "hooks";
 const App = () => {
   const initRef = useRef<boolean>(false);
 
-  const { data, filteredData, fetchAnalyzedData } = useGlobalData();
+  const { data, filteredData, fetchAnalyzedData, loading, setLoading } =
+    useGlobalData();
+
+  const loaderStyle: CSSProperties = {
+    position: "fixed",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, 0)",
+  };
 
   const ideAdapter = container.resolve<IDEPort>("IDEAdapter");
 
   if (initRef.current === false) {
+    setLoading(!loading);
     ideAdapter.addAllEventListener(fetchAnalyzedData);
     ideAdapter.sendFetchAnalyzedDataCommand();
     initRef.current = true;
   }
 
   if (!data?.length) {
-    return <div>NO COMMIT EXISTS YET</div>;
+    return (
+      <BounceLoader
+        color="#ff8272"
+        loading={loading}
+        cssOverride={loaderStyle}
+      />
+    );
   }
 
   return (
