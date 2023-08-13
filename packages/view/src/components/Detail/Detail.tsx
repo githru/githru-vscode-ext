f1b2ddb4a7822968af000703114690c8b415ca2f
rlaqltmxm <rlaqltmxm@gmail.com>
2023-08-13 21:58:22 +0900

    config: Add svg loader for webpack

--- packages/view/src/components/Detail/Detail.tsx
+++ packages/view/src/components/Detail/Detail.tsx
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 
-import { ReactComponent as AuthorIcon } from "../../assets/author.svg";
-import { ReactComponent as ChangedFileIcon } from "../../assets/changed-file.svg";
-import { ReactComponent as CommitIcon } from "../../assets/commit.svg";
-import { ReactComponent as DiffAddIcon } from "../../assets/diff-add.svg";
-import { ReactComponent as DiffDeleteIcon } from "../../assets/diff-delete.svg";
+import { ReactComponent as AuthorIcon } from "assets/author.svg";
+import { ReactComponent as ChangedFileIcon } from "assets/changed-file.svg";
+import { ReactComponent as CommitIcon } from "assets/commit.svg";
+import { ReactComponent as DiffAddIcon } from "assets/diff-add.svg";
+import { ReactComponent as DiffDeleteIcon } from "assets/diff-delete.svg";
 
 import { useCommitListHide } from "./Detail.hook";
 import { getCommitListDetail } from "./Detail.util";
