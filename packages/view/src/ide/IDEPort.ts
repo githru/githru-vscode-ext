cbe1dd4a70d3f6c888f59f01f68b6d5165093f39
ytaek <ytaek.kim@hcil.snu.ac.kr>
2023-08-17 00:18:16 +0900

    refactor(view): rename messages, abstract port usages. (#383)
    * refactor(view): rename messages, abstract port usages.
    
    * fix: fix build failure

--- packages/view/src/ide/IDEPort.ts
+++ packages/view/src/ide/IDEPort.ts
@@ -1,4 +1,4 @@
-﻿import type { ClusterNode } from "types";
+﻿import type { IDESentEvents } from "types/IDESentEvents";
 
 export type IDEMessage = {
   command: string;
@@ -6,7 +6,7 @@ export type IDEMessage = {
 };
 
 export default interface IDEPort {
-  addAllEventListener: (fetchAnalyzedData: (analyzedData: ClusterNode[]) => void) => void;
-  sendFetchAnalyzedDataCommand: () => void;
+  addIDESentEventListener: (apiCallbacks: IDESentEvents) => void;
+  sendFetchAnalyzedDataMessage: () => void;
   setPrimaryColor: (color: string) => void;
 }
--- packages/view/src/types/EngineCommand.ts
+++
@@ -1,6 +0,0 @@
-export type EngineCommand = {
-  command: EngineCommandNames;
-  payload?: string;
-};
-
-export type EngineCommandNames = "fetchAnalyzedData" | "getBranchList" | "updatePrimaryColor";
