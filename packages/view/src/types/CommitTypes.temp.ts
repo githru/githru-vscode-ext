42924a30e6bc61e29163e0d99d4aeab099e7f8b8
root <root@DESKTOP-J22D89D>
2023-08-30 21:20:16 +0900

    refactor: CommitTypes와 NodeTypes 분리

---
+++ packages/view/src/types/CommitTypes.temp.ts
@@ -0,0 +1,51 @@
+// TODO: Entire types will be imported from analysis-engine
+export const CommitMessageTypeList = [
+  "build",
+  "chore",
+  "ci",
+  "docs",
+  "feat",
+  "fix",
+  "pert",
+  "refactor",
+  "revert",
+  "style",
+  "test",
+  "", // default - 명시된 타입이 없거나 commitLint rule을 따르지 않은 경우
+];
+
+const COMMIT_MESSAGE_TYPE = [...CommitMessageTypeList] as const;
+
+export type CommitMessageType = (typeof COMMIT_MESSAGE_TYPE)[number];
+
+// todo: engine DifferenceStatistic 와 통일
+export type DiffStatistics = {
+  changedFileCount: number;
+  insertions: number;
+  deletions: number;
+  files: {
+    [id: string]: {
+      insertions: number;
+      deletions: number;
+    };
+  };
+};
+
+// todo: engine GitUser 와 통일
+export type GitHubUser = {
+  id: string;
+  names: string[];
+  emails: string[];
+};
+
+export type Commit = {
+  id: string;
+  parentIds: string[];
+  author: GitHubUser;
+  committer: GitHubUser;
+  authorDate: string;
+  commitDate: string;
+  diffStatistics: DiffStatistics;
+  message: string;
+  // fill necessary properties...
+};
