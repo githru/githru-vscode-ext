63626c2e481f5fc645dafb78947ee6fedabc26d2
root <root@DESKTOP-J22D89D>
2023-08-30 21:56:02 +0900

    refactor: remove temp

--- packages/view/src/types/index.ts
+++ packages/view/src/types/index.ts
@@ -1,5 +1,5 @@
 export * from "./global";
-export * from "./NodeTypes.temp";
+export * from "./NodeTypes";
 export * from "./IDESentEvents";
 export * from "./IDEMessage";
 export * from "./Author";
