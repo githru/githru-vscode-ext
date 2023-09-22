7d9885edeaee394f8d7a17a073ef965c01a7243d
root <root@DESKTOP-J22D89D>
2023-09-23 02:37:17 +0900

    fix: payload-type error 임시로 방어 로직을 넣었는데 근본적으로 payload가 하나의 타입으로 들어와야함

--- packages/view/src/ide/FakeIDEAdapter.ts
+++ packages/view/src/ide/FakeIDEAdapter.ts
@@ -14,7 +14,21 @@ export default class FakeIDEAdapter implements IDEPort {
     const onReceiveMessage = (e: IDEMessageEvent): void => {
       const responseMessage = e.data;
       const { command, payload } = responseMessage;
-      const payloadData = payload ? JSON.parse(payload) : undefined;
+      let payloadData;
+
+      // payload에서 string으로 올때 있고 object로 올때 있음 이건 잡아줘야하는 버그인데 당분간 못 잡지 않을까 ...
+      if (typeof payload === "string") {
+        try {
+          payloadData = JSON.parse(payload);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          payloadData = payload; // JSON 파싱 실패시 원래 문자열 사용
+        }
+      } else if (typeof payload === "object") {
+        payloadData = payload; // 이미 객체인 경우 그대로 사용
+      } else {
+        payloadData = undefined; // 그 외의 경우, undefined로 설정
+      }
 
       switch (command) {
         case "fetchAnalyzedData":
