7d0498211c1c3aceebfb731441a161cb9388c48d
xxxjinn <tlsguswls319@naver.com>
2024-08-01 23:48:27 +0900

    feature: add authors title

--- packages/view/src/components/FilteredAuthors/FilteredAuthors.tsx
+++ packages/view/src/components/FilteredAuthors/FilteredAuthors.tsx
@@ -12,18 +12,21 @@ const FilteredAuthors = () => {
 
   return (
     <div className="selected-container">
-      {authSrcMap &&
-        selectedClusters.map((selectedCluster) => {
-          return selectedCluster.summary.authorNames.map((authorArray: string[]) => {
-            return authorArray.map((authorName: string) => (
-              <Author
-                key={authorName}
-                name={authorName}
-                src={authSrcMap[authorName]}
-              />
-            ));
-          });
-        })}
+      {selectedClusters.length > 0 && <p>Authors:</p>}
+      <div className="selected-content">
+        {authSrcMap &&
+          selectedClusters.map((selectedCluster) => {
+            return selectedCluster.summary.authorNames.map((authorArray: string[]) => {
+              return authorArray.map((authorName: string) => (
+                <Author
+                  key={authorName}
+                  name={authorName}
+                  src={authSrcMap[authorName]}
+                />
+              ));
+            });
+          })}
+      </div>
     </div>
   );
 };
