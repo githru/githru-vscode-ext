2dd131b8caefcc900be04034512f02d9ec3803eb
xxxjinn <tlsguswls319@naver.com>
2024-08-14 15:55:10 +0900

    style: styling branch selector with mui

--- packages/view/src/components/BranchSelector/BranchSelector.tsx
+++ packages/view/src/components/BranchSelector/BranchSelector.tsx
@@ -1,36 +1,52 @@
-import { type ChangeEventHandler } from "react";
-import "./BranchSelector.scss";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import type { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 
 import { useGlobalData } from "hooks";
 import { sendFetchAnalyzedDataCommand } from "services";
+import "./BranchSelector.scss";
 
 const BranchSelector = () => {
   const { branchList, selectedBranch, setSelectedBranch, setLoading } = useGlobalData();
 
-  const handleChangeSelect: ChangeEventHandler<HTMLSelectElement> = (e) => {
-    setSelectedBranch(e.target.value);
+  const handleChangeSelect = (event: SelectChangeEvent) => {
+    setSelectedBranch(event.target.value);
     setLoading(true);
-    sendFetchAnalyzedDataCommand(e.target.value);
+    sendFetchAnalyzedDataCommand(event.target.value);
   };
 
   return (
-    <section className="branch-selector">
-      <span>Branches:</span>
-      <select
-        className="select-box"
-        onChange={handleChangeSelect}
-        value={selectedBranch}
+    <div className="branch-selector">
+      <p>Branches: </p>
+      <FormControl
+        sx={{ m: 1, minWidth: 120 }}
+        size="small"
       >
-        {branchList?.map((option) => (
-          <option
-            key={option}
-            value={option}
-          >
-            {option}
-          </option>
-        ))}
-      </select>
-    </section>
+        <InputLabel id="branch-select-small-label">Branches</InputLabel>
+        <Select
+          labelId="branch-select-small-label"
+          id="branch-select-small"
+          value={selectedBranch}
+          label="Branches"
+          onChange={handleChangeSelect}
+          className="select-box"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {branchList?.map((option) => (
+            <MenuItem
+              key={option}
+              value={option}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
   );
 };
 
