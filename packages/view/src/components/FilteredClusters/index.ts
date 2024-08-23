0cd5ae63e99380d1119be3e00ae203af6286e937
MINJEONG <lxxmnmn@gmail.com>
2024-08-23 14:38:35 +0900

    feat(view): 선택된 클러스터 드롭다운 리스트 추가

---
+++ packages/view/src/components/FilteredClusters/index.ts
@@ -0,0 +1,1 @@
+export { default as FilteredClusters } from "./FilteredClusters";
--- packages/view/src/components/VerticalClusterList/VerticalClusterList.tsx
+++ packages/view/src/components/VerticalClusterList/VerticalClusterList.tsx
@@ -1,13 +1,22 @@
-import "./VerticalClusterList.scss";
-
+import { useGlobalData } from "hooks";
 import { FilteredAuthors } from "components/FilteredAuthors";
+import { FilteredClusters } from "components/FilteredClusters";
 
 import { Summary } from "./Summary";
 
+import "./VerticalClusterList.scss";
+
 const VerticalClusterList = () => {
+  const { selectedData } = useGlobalData();
+
   return (
     <div className="vertical-cluster-list">
-      <FilteredAuthors />
+      {selectedData.length > 0 && (
+        <div className="selected__container">
+          <FilteredAuthors />
+          <FilteredClusters />
+        </div>
+      )}
       <Summary />
     </div>
   );
