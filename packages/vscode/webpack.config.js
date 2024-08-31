ff36884dc58e8d6e942e4c97ab1011299fcb331b
Madang Garden <chooi9522@gmail.com>
2024-09-01 00:20:46 +0900

    chore/#634 : webpack 설정 제외

--- packages/vscode/webpack.config.js
+++ packages/vscode/webpack.config.js
@@ -23,10 +23,6 @@ const extensionConfig = {
     vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
   },
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/,
-  },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: [".ts", ".js"],
