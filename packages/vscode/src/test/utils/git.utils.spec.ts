2f1d8055c27ce0d96d98a4bbfa8db1735cb1470c
kimyh93 <kimyh93@rexgen.co.kr>
2024-09-02 21:31:34 +0900

    chore: getGitLog 관련 테스트 코드 수정
    Desc
    - 지정한 ssh 를 활용하여 clone 받은 후 테스트에 활용하도록 로직 수정
    - 테스트 수행 후  clone 받은 파일 제거

--- packages/vscode/src/test/utils/git.utils.spec.ts
+++ packages/vscode/src/test/utils/git.utils.spec.ts
@@ -1,49 +1,30 @@
 import * as cp from "child_process";
+import * as fs from "fs";
+import * as path from "path";
 
 import { getGitLog } from "../../utils/git.util";
 
-jest.mock("child_process");
-const mockSpawn = cp.spawn as jest.Mock;
+const SAMPLE_REPO_SSH = "git@github.com:nginx/nginx.git";
+const repoPath = path.resolve(__dirname, "test-repo");
 
-mockSpawn.mockImplementation(() => {
-  const cmd = {
-    stdout: {
-      on: jest.fn().mockImplementation((event, callback) => {
-        if (event === "data") {
-          callback(Buffer.from("commit 1234567\nAuthor: Test User\nDate: Today\n\n    Initial commit"));
-        }
-        if (event === "close") {
-          callback(); // stdout 스트림이 종료되면 호출됨
-        }
-      }),
-    },
-    stderr: {
-      on: jest.fn().mockImplementation((event, callback) => {
-        if (event === "data") {
-          callback("mocked error message");
-        }
-        if (event === "close") {
-          callback(); // stderr 스트림이 종료되면 호출됨
-        }
-      }),
-    },
-    on: jest.fn().mockImplementation((event, callback) => {
-      if (event === "exit") {
-        callback(0); // 프로세스가 정상적으로 종료되었음을 나타냄
-      }
-    }),
-  };
-  return cmd;
+const setDeleteTestRepo = () => {
+  if (fs.existsSync(repoPath)) {
+    fs.rmSync(repoPath, { recursive: true, force: true });
+  }
+};
+
+beforeEach(() => {
+  setDeleteTestRepo();
+  cp.execSync(`git clone ${SAMPLE_REPO_SSH} ${repoPath}`, { stdio: "inherit" });
+});
+
+afterEach(() => {
+  setDeleteTestRepo();
 });
 
 describe("getGitLog with actual resolveSpawnOutput", () => {
   it("should return git log output", async () => {
-    console.time("getGitLog Performance");
-    const result = await getGitLog("/usr/bin/git", "/path/to/repo");
-    console.timeEnd("getGitLog Performance");
-
-    expect(result).toContain("commit 1234567");
-    expect(result).toContain("Author: Test User");
-    expect(result).toContain("Initial commit");
+    const result = await getGitLog("git", repoPath);
+    expect(result).toContain("commit");
   });
 });
