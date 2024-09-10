e93496d7d4addcf3c8e147b854b617c129390f0a
MINJEONG <lxxmnmn@gmail.com>
2024-09-10 17:25:07 +0900

    refactor(view): apply BEM naming in ClusterGraph component

--- packages/view/src/components/VerticalClusterList/ClusterGraph/Draws/drawCommitAmountCluster.ts
+++ packages/view/src/components/VerticalClusterList/ClusterGraph/Draws/drawCommitAmountCluster.ts
@@ -10,7 +10,7 @@ export const drawCommitAmountCluster = (
   const widthScale = d3.scaleLinear().range([0, graphHeight]).domain([0, 10]);
   container
     .append("rect")
-    .attr("class", "cluster-graph-container__commit-amount-cluster")
+    .attr("class", "cluster-graph__commit-amount")
     .attr("width", (d) => widthScale(Math.min(d.clusterSize, 10)))
     .attr("height", clusterHeight)
     .attr("x", (d) => (graphHeight - widthScale(Math.min(d.clusterSize, 10))) / 2);
