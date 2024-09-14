da96470ab6f18aff51a8d1a624beda232f1d40ec
pithesun <wonjh0730@naver.com>
2024-09-14 14:19:05 +0900

    feat(view): getCommitLatestTag 함수 관련 방어로직 추가(#361)

--- packages/view/src/components/VerticalClusterList/Summary/Summary.util.ts
+++ packages/view/src/components/VerticalClusterList/Summary/Summary.util.ts
@@ -3,31 +3,62 @@ import { getAuthorProfileImgSrc } from "utils/author";
 
 import type { AuthSrcMap, Cluster } from "./Summary.type";
 
-function tagToNumber(tag: string, maxLength: number) {
+/**
+ * 릴리스 테그 형식이 맞는지 유효성을 확인하다.
+ * @param tag
+ * @returns
+ */
+const isValidReleaseTag = (tag: string): boolean => {
+  const isValidTag = /^v\d+(\.\d+)*$/;
+
+  if (isValidTag.test(tag)) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * 태그의 버전 이름을 숫자로 바꾼다.
+ * ex) v.1.1.1 -> 111
+ * @param tag
+ * @param maxLength
+ * @returns number
+ *
+ */
+function tagToNumber(tag: string, maxLength: number): number {
+  if (!isValidReleaseTag(tag)) return 0;
   return Number(tag.replace(/[v.]/g, "").padEnd(maxLength, "0"));
 }
 
-// 클러스터 커밋들의 태그들을 비교해서 가장 최근의 tag version을 가져온다.
-function getCommitLatestTag(tags: string[]) {
-  let latestTag = "";
+/**
+ * 클러스터 커밋들의 태그들을 비교해서 가장 최근의 tag version을 가져온다.
+ * @param tags
+ * @returns
+ */
+function getCommitLatestTag(tags: string[]): string {
+  if (!Array.isArray(tags) || tags.length === 0) return "";
 
-  if (tags) {
-    const maxTagLength = tags.reduce((maxLength, tag) => {
-      const tmp = tag.replace(/[v.]/g, "");
-      return maxLength <= tmp.length ? tmp.length : maxLength;
-    }, 0);
+  const validTags = tags.filter((tag) => isValidReleaseTag(tag));
+  if (validTags.length === 0) return "";
 
-    let tagFormatNum = 0;
-    const latestTagIndex = tags.reduce((latestTagIdx, tag, idx) => {
-      const currnetTagNum = tagToNumber(tag, maxTagLength);
-      if (currnetTagNum >= tagFormatNum) {
-        tagFormatNum = currnetTagNum;
-        return idx;
-      }
-      return latestTagIdx;
-    }, 0);
-    latestTag = tags[latestTagIndex];
-  }
+  const maxTagLength = validTags.reduce((maxLength, tag) => {
+    const tmp = tag.replace(/[v.]/g, "");
+    return maxLength <= tmp.length ? tmp.length : maxLength;
+  }, 0);
+
+  let prevTagNumber = 0;
+  const latestTagIndex = validTags.reduce((latestTagIdx, tag, idx) => {
+    const currnetTagNumber = tagToNumber(tag, maxTagLength);
+    if (currnetTagNumber >= prevTagNumber) {
+      prevTagNumber = currnetTagNumber;
+      return idx;
+    }
+    return latestTagIdx;
+  }, 0);
+
+  const latestTag = tags[latestTagIndex];
+
   return latestTag;
 }
 
