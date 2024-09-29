a739c3f9023fe1edc1d2bfdbaf4a258a91878040
YongHyunKing <yhkim8246@gmail.com>
2024-09-29 17:54:25 +0900

    [engine] #713 요구사항 적용

--- packages/vscode/webpack.config.js
+++ packages/vscode/webpack.config.js
@@ -19,7 +19,6 @@ const extensionConfig = {
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, "dist"),
-        // filename: "extension.js",
         filename: "[name].js",
         libraryTarget: "commonjs2",
     },
