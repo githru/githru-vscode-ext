883453d99863db96c3e8e0cbb19dd6a3d4b8ec93
YongHyunKing <yhkim8246@gmail.com>
2024-10-15 20:46:58 +0900

    [engine] 주석 제거 및 prettier 적용

--- packages/vscode/src/utils/git.util.ts
+++ packages/vscode/src/utils/git.util.ts
@@ -1,11 +1,9 @@
 import { COMMIT_SEPARATOR, GIT_LOG_SEPARATOR } from "@githru-vscode-ext/analysis-engine";
 import * as cp from "child_process";
 import * as fs from "fs";
-import os from 'os';
+import os from "os";
 import * as path from "path";
-import { Worker } from 'worker_threads';
-
-
+import { Worker } from "worker_threads";
 
 export interface GitExecutable {
   readonly path: string;
@@ -204,78 +202,70 @@ export async function getGitLog(gitPath: string, currentWorkspacePath: string): 
 }
 
 export async function getLogCount(gitPath: string, currentWorkspacePath: string): Promise<number> {
-    const BASE_10 = 10;
-    return new Promise((resolve, reject) => {
-        const args = [
-        "rev-list",
-        "--count",
-        "--all",
-        ];
+  const BASE_10 = 10;
+  return new Promise((resolve, reject) => {
+    const args = ["rev-list", "--count", "--all"];
 
     resolveSpawnOutput(
-        cp.spawn(gitPath, args, {
-            cwd: currentWorkspacePath,
-            env: Object.assign({}, process.env),
-        })
-        ).then(([status, stdout, stderr]) => {
-        const { code, error } = status;
+      cp.spawn(gitPath, args, {
+        cwd: currentWorkspacePath,
+        env: Object.assign({}, process.env),
+      })
+    ).then(([status, stdout, stderr]) => {
+      const { code, error } = status;
 
-        if (code === 0 && !error) {
-            const commitCount = parseInt(stdout.toString().trim(), BASE_10); // Buffer를 문자열로 변환 후 숫자로 변환
-            resolve(commitCount); // 숫자를 반환
-        } else {
-            reject(stderr);
-        }
-        });
+      if (code === 0 && !error) {
+        const commitCount = parseInt(stdout.toString().trim(), BASE_10);
+        resolve(commitCount);
+      } else {
+        reject(stderr);
+      }
     });
+  });
 }
-
 
 export async function fetchGitLogInParallel(gitPath: string, currentWorkspacePath: string): Promise<string> {
   const numCores = os.cpus().length;
-  
+
   const totalCnt = await getLogCount(gitPath, currentWorkspacePath);
   let numberOfThreads = 1;
 
   const taskThreshold = 1000;
   const coreCountThreshold = 4;
 
-if (totalCnt > taskThreshold) {
-  if (numCores < coreCountThreshold) numberOfThreads = 2;
-  else numberOfThreads = 3;
-}
- 
-  const chunkSize = Math.ceil(totalCnt/ numberOfThreads);
+  if (totalCnt > taskThreshold) {
+    if (numCores < coreCountThreshold) numberOfThreads = 2;
+    else numberOfThreads = 3;
+  }
+
+  const chunkSize = Math.ceil(totalCnt / numberOfThreads);
   const promises: Promise<string>[] = [];
 
   for (let i = 0; i < numberOfThreads; i++) {
     const skipCount = i * chunkSize;
     const limitCount = chunkSize;
 
-    console.log('__dirname:', __dirname);
-    const worker = new Worker(path.resolve(__dirname, './worker.js'), {
+    const worker = new Worker(path.resolve(__dirname, "./worker.js"), {
       workerData: {
         gitPath,
         currentWorkspacePath,
         skipCount,
         limitCount,
-        COMMIT_SEPARATOR, 
-        GIT_LOG_SEPARATOR 
+        COMMIT_SEPARATOR,
+        GIT_LOG_SEPARATOR,
       },
     });
 
     promises.push(
       new Promise((resolve, reject) => {
-        worker.on('message', resolve);
-        worker.on('error', reject);
+        worker.on("message", resolve);
+        worker.on("error", reject);
       })
     );
   }
 
-  return Promise.all(promises).then((logs) => logs.join('\n'));
+  return Promise.all(promises).then((logs) => logs.join("\n"));
 }
-
-
 
 export async function getGitConfig(
   gitPath: string,
--- packages/vscode/src/utils/git.worker.ts
+++ packages/vscode/src/utils/git.worker.ts
@@ -1,60 +1,59 @@
-import * as cp from 'child_process';
-import { parentPort, workerData } from 'worker_threads';
+import * as cp from "child_process";
+import { parentPort, workerData } from "worker_threads";
 
-import { resolveSpawnOutput } from './git.util'
+import { resolveSpawnOutput } from "./git.util";
 
-const { gitPath, currentWorkspacePath, skipCount, limitCount,COMMIT_SEPARATOR,GIT_LOG_SEPARATOR } = workerData;
-
-    
-
+const { gitPath, currentWorkspacePath, skipCount, limitCount, COMMIT_SEPARATOR, GIT_LOG_SEPARATOR } = workerData;
 
 async function getPartialGitLog() {
-    const gitLogFormat =
-      COMMIT_SEPARATOR +
-      [
-        "%H", // commit hash (id)
-        "%P", // parent hashes
-        "%D", // ref names (branches, tags)
-        "%an", // author name
-        "%ae", // author email
-        "%ad", // author date
-        "%cn",
-        "%ce",
-        "%cd", // committer name, committer email and committer date
-        "%B", // commit message  (subject and body)
-      ].join(GIT_LOG_SEPARATOR) +
-      GIT_LOG_SEPARATOR;
+  const gitLogFormat =
+    COMMIT_SEPARATOR +
+    [
+      "%H", // commit hash (id)
+      "%P", // parent hashes
+      "%D", // ref names (branches, tags)
+      "%an", // author name
+      "%ae", // author email
+      "%ad", // author date
+      "%cn",
+      "%ce",
+      "%cd", // committer name, committer email and committer date
+      "%B", // commit message  (subject and body)
+    ].join(GIT_LOG_SEPARATOR) +
+    GIT_LOG_SEPARATOR;
 
-    const args = [
-        '--no-pager',
-        'log',
-        '--all',
-        '--parents',
-        '--numstat',
-        '--date-order',
-        `--pretty=format:${gitLogFormat}`,
-        '--decorate',
-        '-c',
-        `--skip=${skipCount}`,
-        `-n ${limitCount}`,
-    ];
+  const args = [
+    "--no-pager",
+    "log",
+    "--all",
+    "--parents",
+    "--numstat",
+    "--date-order",
+    `--pretty=format:${gitLogFormat}`,
+    "--decorate",
+    "-c",
+    `--skip=${skipCount}`,
+    `-n ${limitCount}`,
+  ];
 
-    resolveSpawnOutput(
-        cp.spawn(gitPath, args, {
-            cwd: currentWorkspacePath,
-            env: Object.assign({}, process.env),
-        })
-        ).then(([status, stdout, stderr]) => {
-        const { code, error } = status;
+  resolveSpawnOutput(
+    cp.spawn(gitPath, args, {
+      cwd: currentWorkspacePath,
+      env: Object.assign({}, process.env),
+    })
+  )
+    .then(([status, stdout, stderr]) => {
+      const { code, error } = status;
 
-        if (code === 0 && !error && parentPort !== null) {
-            parentPort.postMessage(stdout.toString());
-        } else {
-            if (parentPort !== null) parentPort.postMessage(stderr);
-        }
-        }).catch(error => {
-            console.error('Spawn Error:', error);
-        });
+      if (code === 0 && !error && parentPort !== null) {
+        parentPort.postMessage(stdout.toString());
+      } else {
+        if (parentPort !== null) parentPort.postMessage(stderr);
+      }
+    })
+    .catch((error) => {
+      console.error("Spawn Error:", error);
+    });
 }
 
 getPartialGitLog();
